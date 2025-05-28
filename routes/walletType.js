f72ad8a0d94150bc65a77672c77304dbc0c2b7b6
DMHPJ <2020625277@qq.com>
2025-05-28 16:08:36 +0800

    删除旧版本文件

--- routes/walletType.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-var walletTypeController = require("../controllers/walletTypeController");
-var route = express.Router();
-
-route.post("/list", walletTypeController.getWalletTypeList);
-
-module.exports = route;
