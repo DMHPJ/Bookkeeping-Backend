da373a700acb78010018d63147ef86030aa50ed0
DMHPJ <2020625277@qq.com>
2025-06-17 15:19:16 +0800

    删除注释

--- src/services/walletService.ts
+++ src/services/walletService.ts
@@ -6,17 +6,13 @@ export class WalletService {
 
 	constructor() {
 		this.walletRepository = new WalletRepository();
-		console.log("service", this)
 	}
 
 	getWalletList = async (params: any): Promise<Wallet[]> => {
-    console.log("service", this)
 		return await this.walletRepository.getWalletList(params);
 	}
 
 	addWallet = async (walletData: Partial<Wallet>): Promise<Wallet> => {
-    console.log("service", this);
-    console.log("service", walletData);
 		return await this.walletRepository.addWallet(walletData);
 	}
 
